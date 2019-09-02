10912af0ce496ace2500facbc4ef34287c8c3095
fred <domaine75@gmail.com>
2019-09-02 23:24:30 +0200

    initial commit

---
+++ MultiGitClone/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace MultiGitClone
+{
+  class Program
+  {
+    static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display("Multi GIT Clone");
+      var urlGitClone = "https://github.com/francoistonic/programmez";
+      var gitCloneStartNumber = 232;
+      var gitCloneEndNumber = 242;
+      display($"Starting to clone from {gitCloneStartNumber}");
+      for (int i = gitCloneStartNumber; i <= gitCloneEndNumber; i++)
+      {
+        if (File.Exist(ExtractUrl(urlGitClone)))
+        {
+          // start a git update
+        }
+        else
+        {
+          // create a git clone url
+
+        }
+      }
+
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    public static string ExtractUrl(string url)
+    {
+      var tmpList = url.Split('/');
+      return tmpList[tmpList.Length];
+    }
+  }
+}
